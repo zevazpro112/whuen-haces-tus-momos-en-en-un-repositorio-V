eb62b43b3be9df0111df2f16c4893d16146553e5
zevazpro112 <164041695+zevazpro112@users.noreply.github.com>
2024-03-19 10:26:21 -0500

    Create dicionario_de_momos.py
    :V

---
+++ dicionario_de_momos.py
@@ -0,0 +1,11 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "XD": "una respuesta comun a algo gracioso, y parece una cara riendo"
+            }
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("lo siento, la palabra que busca no esta por el momento")
